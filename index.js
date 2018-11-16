38893e58a1204d06e1e5c4602ace143b17e3dc4e
Anna Kaplan <annakaplanart@gmail.com>
2018-11-16 13:11:33 -0500

    fixed-index.js-fixed post request for docker

--- index.js
+++ index.js
@@ -14,8 +14,38 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+
 // Constants
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8000
+// helper functions 
+function appendFiles(coded, tested) {
+  //turn the test which is a buffer into string
+  const codeTest = 'code-test.js'
+  coded = coded + '\n'
+  // tested = tested.toString()
+  fs.appendFileSync('code-test.js', coded, 'utf8', err => {
+    if (err) throw err
+  })
+  fs.appendFileSync('code-test.js', tested, 'utf8', err => {
+    if (err) throw err
+  })
+  return codeTest
+}
+async function executeFile(){
+  try {
+    const {stdout} = await execFile('mocha', ['code-test.js'])
+    let files = stdout
+    console.log('this is STDOUT', stdout)
+    console.log("typeOf File", typeof files)
+    return files
+  } catch (err) {
+    let files = err.stdout
+    console.log("ERROR",err.stdout);
+    console.log("typeOf File", typeof files)
+    return files
+  }
+}
+
 
 // App
 app.get('/', (req, res) => {
@@ -23,61 +53,18 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', async (req, res, next) => {
-  async function appendFiles(coded, tested) {
-    coded = coded + '\n'
-    let files
-    await fs.appendFileSync('code-test.js', coded, 'utf8')
-    await fs.appendFileSync('code-test.js', tested, 'utf8')
-
-    async function getVersion() {
-      try {
-        const {stdout} = await execFile('mocha', ['code-test.js'])
-
-        files = stdout
-        console.log('this is STDOUT', stdout)
-
-        return files
-      } catch (err) {
-        files = err.stdout
-
-        //console.log(err.stdout);
-        return files
-      }
-    }
-    let getfiles = await getVersion()
-    return getfiles
+  try{
+    console.log('REQ BODY',req.body)
+    const file = await appendFiles(req.body.code, req.body.test)
+    let testResult = await executeFile()
+    res.json(testResult);
   }
-  try {
-    console.log('REQBODYHEEEE', req.body)
-    // let bufferOriginal1 = Buffer.from(req.body.code)
-    // let bufferOriginal2 = Buffer.from(req.body.test)
-    // let code = bufferOriginal1.toString('utf8')
-    // let test = bufferOriginal2.toString('utf8')
-    // console.log('thsi is code', code)
-    // let respond = await appendFiles(code, test)
-    console.log('thsi is the type of code in req', typeof req.body.code)
-    let code = req.body.code.replace(/\n/g, ' ')
-    let test = req.body.test.replace(/\n/g, ' ')
-    console.log('reqex code???????', code)
-    let respond = await appendFiles(code, test)
-    //let respond = await appendFiles(req.body.code, req.body.test)
-    console.log('RESPONDTOJSON', respond)
-    res.json(respond)
-  } catch (err) {
-    console.log(err)
+  catch (err){
+    console.error(err)
     next(err)
   }
 })
-
-// app.use((req, res, next) => {
-//   if (path.extname(req.path).length) {
-//     const err = new Error('Not found')
-//     err.status = 404
-//     next(err)
-//   } else {
-//     next()
-//   }
-// })
+  
 
 app.use((err, req, res, next) => {
   console.error(err)
